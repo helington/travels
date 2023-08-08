94bf14e16486962dbc63b70ac09c29442dc5f1f2
heisson.lima <heisson.lima@celus.io>
2023-08-08 21:52:03 +0200

    feature: geração de PDF

--- fretes/admin.py
+++ fretes/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+
 from .models import Frete
 
 # Register your models here.
--- fretes/views.py
+++ fretes/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
+from django.shortcuts import render
 from django.urls import reverse
+from reportlab.lib.pagesizes import letter
+from reportlab.lib.styles import getSampleStyleSheet
+from reportlab.platypus import Paragraph, SimpleDocTemplate, Spacer
+
 from .models import Frete
 
 # Create your views here.
@@ -24,22 +28,46 @@ def organizeFreights():
     freightsPerMonth = {}
     for i in range(1, 13):
         freightsInMonth = Frete.objects.filter(date__month=i)
-        if len(freightsInMonth) != 0:
-            print(len(freightsInMonth))
+        if freightsInMonth:
             freightsPerMonth[months[i - 1]] = freightsInMonth
     return freightsPerMonth
-
-
-for key, value in organizeFreights().items():
-    print(key)
-    for freight in value:
-        print(value)
-    print
 
 
 def index(request):
     months = organizeFreights
     return render(request, "fretes/index.html", {"months": months})
+
+
+def generate_pdf(request):
+    freights = organizeFreights()
+    month_names = list(freights.keys())
+
+    response = HttpResponse(content_type="application/pdf")
+    response[
+        "Content-Disposition"
+    ] = f'attachment; filename="Fretes de {month_names[0]} a {month_names[1]}.pdf"'
+    # NOTE: Isso vai quebrar se o banco de dados estiver vazio.
+    # O ideal é não mostrar o botão de renderizar o PDF nesse caso.
+    # Caso essa função seja chamada mesmo nesse caso, `month_names` vazio ainda precisa ser tratado
+    #
+    # TODO: Tratar caso de `month_names` vazio
+    # TODO: Adicionar datas no arquivo PDF
+
+    doc = SimpleDocTemplate(response, pagesize=letter)
+    styles = getSampleStyleSheet()
+
+    pdf_content = []
+    for month, freights_in_month in freights.items():
+        heading = Paragraph(f"<b>{month}</b>", styles["Heading1"])
+        pdf_content.append(heading)
+
+        for freight in freights_in_month:
+            pdf_content.append(Paragraph(f"• {freight.title}", styles["Normal"]))
+
+        pdf_content.append(Spacer(0, 10))
+
+    doc.build(pdf_content)
+    return response
 
 
 def removeFreight(request, freight_id):
