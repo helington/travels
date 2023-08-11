53a5eb25cff1ae7e89468d3463b25dbe6c07c921
helington <helington27@gmail.com>
2023-08-11 07:13:07 -0300

    Novo modelo: Contato

--- fretes/admin.py
+++ fretes/admin.py
@@ -1,6 +1,7 @@
 from django.contrib import admin
 
-from .models import Frete
+from .models import Frete, Contato
 
 # Register your models here.
 admin.site.register(Frete)
+admin.site.register(Contato)
--- fretes/models.py
+++ fretes/models.py
@@ -12,3 +12,14 @@ class Frete(models.Model):
 
     def makeDateForHTML(self):
         return f"{self.date.year}-{self.date.strftime('%m')}-{self.date.day}"
+
+class Contato(models.Model):
+    name = models.CharField(max_length=64)
+    phone_number = models.CharField(max_length=11)
+
+    def __str__(self):
+        return f"""
+        id: {self.id}
+        name: {self.name}
+        phone_number: {self.phone_number}
+        """
